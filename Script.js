bcd63b57f3c84bc23790bd96a934fc1564cbf363
Romans122 <lm_lang@yahoo.com>
2015-09-29 14:00:24 -0400

    Refined_and_filled_pages_pictures_fixed_links_navbar

---
+++ Script.js
@@ -0,0 +1,9 @@
+document.getElementById("nav01html").innerHTML =
+"<ul id='myNavbar'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='About.html'>About</a></li>" +
+"<li><a href='Facts.html'>Facts</a></li>" +
+"<li><a href='Devotions.html'>Devotions</a></li>" +
+"<li><a href='Contact.html'>Contact</a></li>" +
+
+"</ul>";
